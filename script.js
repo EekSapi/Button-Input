53bebc23d12a55c0af52309c6dee7fa2bfcc442a
EekSapi <135517117+EekSapi@users.noreply.github.com>
2023-06-09 12:24:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+// DEFAULT //
+function defaultblur(){
+    document.getElementById('default').style.color = "#828282";
+}
+
+function defaultfocus(){
+    document.getElementById('default').style.color = "#2962FF";
+}
+
+// ERROR //
+function errorhover(){
+    document.getElementById('error').style.color = "#333333";
+}
+
+function errorleave(){
+    document.getElementById('error').style.color = "#D32F2F";
+}
+
+// HELPER //
+function helperblur(){
+    document.getElementById('helper').style.color = "#828282";
+    document.getElementById('special-helper').style.color = "#828282";
+}
+
+function helperfocus(){
+    document.getElementById('helper').style.color = "#2962FF";
+    document.getElementById('special-helper').style.color = "#2962FF";
+}
+
+// HELPER ERROR //
+function helpererrorhover(){
+    document.getElementById('special-error').style.color = "#333333";
+    document.getElementById('special-helper-error').style.color = "#333333";
+}
+
+function helpererrorleave(){
+    document.getElementById('special-error').style.color = "#D32F2F";
+    document.getElementById('special-helper-error').style.color = "#D32F2F";
+}
+
+// START ICON //
+function starticonblur(){
+    document.getElementById('starticon').style.color = "#828282";
+}
+
+function starticonfocus(){
+    document.getElementById('starticon').style.color = "#2962FF";
+}
+
+// END ICON //
+function endiconblur(){
+    document.getElementById('special-end').style.color = "#828282";
+}
+
+function endiconfocus(){
+    document.getElementById('special-end').style.color = "#2962FF";
+}
+
+// TEXT //
+function textblur(){
+    document.getElementById('text').style.color = "#828282";
+}
+
+function textfocus(){
+    document.getElementById('text').style.color = "#2962FF";
+}
+
+// SMALL //
+function smblur(){
+    document.getElementById('sm').style.color = "#828282";
+}
+
+function smfocus(){
+    document.getElementById('sm').style.color = "#2962FF";
+}
+
+// MEDIUM //
+function mdblur(){
+    document.getElementById('special-md').style.color = "#828282";
+}
+
+function mdfocus(){
+    document.getElementById('special-md').style.color = "#2962FF";
+}
+
+// FULL //
+function fullblur(){
+    document.getElementById('full').style.color = "#828282";
+}
+
+function fullfocus(){
+    document.getElementById('full').style.color = "#2962FF";
+}
+
+// MULTI //
+function multiblur(){
+    document.getElementById('multi').style.color = "#828282";
+}
+
+function multifocus(){
+    document.getElementById('multi').style.color = "#2962FF";
+}
